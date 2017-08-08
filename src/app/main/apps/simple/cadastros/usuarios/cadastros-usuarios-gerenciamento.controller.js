e108d90026eecae0ac47ab0a847ac0fd4dfaec53
julianoezequiel <julianoezequiel@mail.com>
2017-08-07 21:41:43 -0300

    cadastro usuario

--- src/app/main/apps/simple/cadastros/usuarios/cadastros-usuarios-gerenciamento.controller.js
+++ src/app/main/apps/simple/cadastros/usuarios/cadastros-usuarios-gerenciamento.controller.js
@@ -21,12 +21,46 @@
     console.log('modulo UsuarioGerenciamentoController');
     console.log(vm.usuario);
 
-    vm.voltar = function(){
+    vm.voltar = function() {
       $rootScope.state.go('app.simple-cadastros-usuarios');
     }
 
     vm.activeMailPaneIndex = 0;
     vm.dynamicHeight = false;
+
+    vm.situacoes = ['Ativo', 'Inativo'];
+    vm.usuario = new Object();
+    vm.usuario.senha = '';
+
+    vm.permissoes =[{
+      id:1,
+      descricao:'Cadastro de Operadores'
+    },{
+      id:2,
+      descricao:'Cadastro de Clientes'
+    },{
+      id:1,
+      descricao:'Cadastro de Operadores'
+    },{
+      id:1,
+      descricao:'Cadastro de Operadores'
+    },{
+      id:1,
+      descricao:'Cadastro de Operadores'
+    },{
+      id:1,
+      descricao:'Cadastro de Operadores'
+    },{
+      id:1,
+      descricao:'Cadastro de Operadores'
+    }]
+
+    vm.items = [1, 2, 3, 4, 5];
+    vm.selected = [];
+
+    vm.toggle = function(item, list) {
+      console.log(item);
+    };
 
   }
 
